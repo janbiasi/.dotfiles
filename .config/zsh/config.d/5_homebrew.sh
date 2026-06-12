87baef99cd48e09f5f41f64b0446e70aeb034074
Jan Biasi <janbiasi@users.noreply.github.com>
2026-06-12 07:47:51 +0200

    feat: update homebrew to v6, add trust settings for taps

--- .config/zsh/config.d/5_homebrew.sh
+++ .config/zsh/config.d/5_homebrew.sh
@@ -1,4 +1,6 @@
 # Load brew shell environment (macOS only)
 if [[ "$OSTYPE" == darwin* ]] && [ -d /opt/homebrew ]; then
+  export HOMEBREW_BUNDLE_FILE="$HOME/.dotfiles/extra/homebrew/Brewfile"
+  export HOMEBREW_REQUIRE_TAP_TRUST=1
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
